8ec841e926f332955d5218f7327580b6e04d1632
poschengband <ckousky@gmail.com>
2015-08-21 10:13:18 -0500

    Crafting: Fix bug when source *identified* prior to being crafted.

--- src/spells_c.c
+++ src/spells_c.c
@@ -356,6 +356,12 @@ void crafting_spell(int cmd, variant *res)
         {
             virtue_add(VIRTUE_ENCHANTMENT, 1);
             calc_android_exp();
+
+            object_aware(o_ptr);
+            ego_aware(o_ptr);
+            object_known(o_ptr);
+            o_ptr->ident |= (IDENT_MENTAL);
+            screen_object(o_ptr, 0L);
         }
         var_set_bool(res, TRUE);
         break;
