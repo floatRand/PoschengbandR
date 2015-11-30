97fce583d9266e605104895f677d71b2d542764b
poschengband <ckousky@gmail.com>
2015-11-30 11:10:50 -0600

    Bump Version to 4.0.1

--- src/defines.h
+++ src/defines.h
@@ -18,8 +18,8 @@
 
 #define VER_MAJOR 4
 #define VER_MINOR 0
-#define VER_PATCH 0
-#define VER_EXTRA 6
+#define VER_PATCH 1
+#define VER_EXTRA 0
 
 #define GAME_MODE_BEGINNER  0
 #define GAME_MODE_NORMAL    1
