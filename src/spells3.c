74ac340828976a384b3e10a7f818797c1e224dc3
elliptic <hyperelliptical@gmail.com>
2016-06-27 22:03:05 -0400

    Reimplement no_selling option.
    Currently just prevents selling with no compensation in gold drops.

--- src/spells3.c
+++ src/spells3.c
@@ -1862,7 +1862,7 @@ bool alchemy(void)
 
     price = obj_value_real(o_ptr);
 
-    if (price <= 0)
+    if (price <= 0 || no_selling)
     {
         msg_format("You turn %s to fool's gold.", o_name);
     }
--- src/variable.c
+++ src/variable.c
@@ -316,6 +316,7 @@ bool ironman_quests; /* Random quests must be completed */
 bool random_artifacts;
 bool no_artifacts;
 bool no_egos;
+bool no_selling;
 bool enable_virtues;
 
 /*** Easy Object Auto-Destroyer ***/
