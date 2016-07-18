58bd659b0760f775006992317fd0df9c296ce711
GenericPseudonym <nottheinferno@gmail.com>
2016-07-18 15:41:24 -0700

    Make auto_id once again affect the player's inventory.
    Before this it would only identify items on the ground.

--- src/dungeon.c
+++ src/dungeon.c
@@ -3415,6 +3415,10 @@ static void process_world(void)
     /* Process recharging */
     process_world_aux_recharge();
 
+    /* Have auto_id affect inventory*/
+    if (p_ptr->auto_id)
+        identify_pack();
+
     /* Feel the inventory */
     sense_inventory1();
     sense_inventory2();
