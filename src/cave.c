b7466b81e0c827eb97e8f63c84eefbf799d06e1c
GenericPseudonym <nottheinferno@gmail.com>
2016-11-29 19:23:01 -0800

    Metamorphosed draconians become D instead of @
    ...but only if display_race is on, of course

--- src/cave.c
+++ src/cave.c
@@ -1378,6 +1378,21 @@ void py_get_display_char_attr(char *c, byte *a)
         }
         if (p_ptr->prace == RACE_MON_RING && p_ptr->riding)
             r_ptr = &r_info[m_list[p_ptr->riding].r_idx];
+        if (p_ptr->prace == RACE_DRACONIAN && mut_present(MUT_DRACONIAN_METAMORPHOSIS))
+        {
+            switch(p_ptr->psubrace)
+            {
+            case DRACONIAN_RED: r_ptr = &r_info[644]; /* Ancient red dragon */
+            case DRACONIAN_WHITE: r_ptr = &r_info[617]; /* etc... */
+            case DRACONIAN_BLUE: r_ptr = &r_info[601];
+            case DRACONIAN_BLACK: r_ptr = &r_info[624];
+            case DRACONIAN_GREEN: r_ptr = &r_info[618];
+            case DRACONIAN_BRONZE: r_ptr = &r_info[MON_ANCIENT_BRONZE_DRAGON];
+            case DRACONIAN_GOLD: r_ptr = &r_info[MON_ANCIENT_GOLD_DRAGON];
+            case DRACONIAN_SHADOW: r_ptr = &r_info[MON_DEATH_DRAKE];
+            case DRACONIAN_CRYSTAL: r_ptr = &r_info[MON_GREAT_CRYSTAL_DRAKE];
+            }
+        }
     }
     else
     {
