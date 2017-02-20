floatRand/PoschengbandR

commit c2a63f438f4efcd047d2feedb0cecfeefde9c49a

Fix targetting (st_).

src/xtra2.c
@@ -3551,11 +3551,11 @@ bool target_okay(void)
             return TRUE;
         }
         /* Position Targets are confusing. They should be dismissed when no longer valid. */
-        target_who = 0;
+        /*target_who = 0;
         target_row = 0;
         target_col = 0;
         p_ptr->redraw |= PR_HEALTH_BARS;
-        return FALSE;
+        return FALSE;*/
     }
 
     /* Check moving targets */
@@ -3576,7 +3576,7 @@ bool target_okay(void)
     }
 
     /* Assume no target */
-    return (FALSE);
+    return (TRUE);
 }
 
 

commit a7a93343cbe3e576f2d4e37b6e51f25edf8ca26a

Fix targeting for real
Commit c2a63f438f4efcd047d2feedb0cecfeefde9c49a broke the abillity to
target "nothing at all" -- target_okay would return true even if the
targeted monster was invalid, or if target_who was 0 and the player
wasn't targeting anything at all!

src/xtra2.c
@@ -3576,22 +3576,7 @@ bool target_able(int m_idx)
 bool target_okay(void)
 {
     /* Accept (projectable) stationary targets */
-    if (target_who < 0)
-    {
-        if ( in_bounds(target_row, target_col)
-          && projectable(py, px, target_row, target_col) )
-        {
-            return TRUE;
-        }
-        /* Position Targets are confusing. They should be dismissed when no longer valid. */
-        /* But out-of-LOS doesn't necessarily mean invalid, so this block gets in the way of gameplay. */
-        /*target_who = 0;
-        target_row = 0;
-        target_col = 0;
-        p_ptr->redraw |= PR_HEALTH_BARS;
-        return FALSE;*/
-    }
-
+    if (target_who < 0) return (TRUE);
     /* Check moving targets */
     if (target_who > 0)
     {
@@ -3610,7 +3595,7 @@ bool target_okay(void)
     }
 
     /* Assume no target */
-    return (TRUE);
+    return (FALSE);
 }
 
 