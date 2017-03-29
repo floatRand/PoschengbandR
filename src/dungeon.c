009ea004f71983f35807ad0850f341ff297a7607
Liiman Saska TTK15SP <saskaliiman@kamk.fi>
2017-03-29 12:07:24 +0300

    Shitty fix to recall in Coffeebreak

--- src/dungeon.c
+++ src/dungeon.c
@@ -2410,10 +2410,9 @@ void process_world_aux_movement(void)
 				/*I feel like I should force player to take on Serpent once he reaches it, but for now, it shall be like this.*/
 				if (coffeebreak_mode)
 				{
-					if (dun_level <= 97) dun_level += 2;
+					if (dun_level <= 97 && quest_number(dun_level+1)==0) dun_level += 2;
 					else if (dun_level == 98) dun_level++;
 				}
-				if (coffeebreak_mode && dun_level <= 98) dun_level++; 
 
                 /* Nightmare mode makes recall more dangerous */
                 if (p_ptr->nightmare_mode && !randint0(666) && (dungeon_type == DUNGEON_ANGBAND))
